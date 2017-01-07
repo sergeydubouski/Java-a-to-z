f738a710d77400218b19ddfee2c590c6b84aa8e7
Sergey Dubouski <sergeydubouski@gmail.com>
2017-01-06 20:16:04 -0500

    Initial commit

---
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -0,0 +1,16 @@
+package ru.job4j;
+
+/**
+ * Calculate class.
+ * @author Sergey Dubouski
+ * @since 04.01.2017
+*/
+class Calculate{
+	/**
+	 * Main.
+	 * @param args - args.
+	*/	
+	public static void main( String args[]){	
+	System.out.println("Hello world!");	
+	}
+}

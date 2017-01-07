36f60e11947afd8e5c2790f18a2dd5196d29ba93
Sergey Dubouski <sergeydubouski@gmail.com>
2017-01-07 16:30:18 -0500

    Added new package-info.java files

--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -12,24 +12,24 @@ import static org.junit.Assert.assertThat;
  * @author Sergey Dubouski
  * @version $Id$
  * @since 0.1
-*/
+ */
 public class CalculateTest {
-	/**
-	 * Test add.
-	*/
-	@Test
-	public void whenAddOneToOneThenTwo(){
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		System.setOut(new PrintStream(out));
-		Calculate.main(null);
-		assertThat(
-				out.toString(),
-				is(
-						String.format(
-								"Hello world!\r\n",
-								System.getProperty("line.Separator")
-						)
-				)
-		);
-	}
+    /**
+     * Test add.
+     */
+    @Test
+    public void whenAddOneToOneThenTwo() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Calculate.main(null);
+        assertThat(
+                out.toString(),
+                is(
+                        String.format(
+                                "Hello world!\r\n",
+                                System.getProperty("line.separator")
+                        )
+                )
+        );
+    }
 }

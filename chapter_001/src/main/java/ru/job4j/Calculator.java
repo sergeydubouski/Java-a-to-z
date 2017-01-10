804f7e602424a31df3f6a1889a084c5c06b591db
Sergey Dubouski <sergeydubouski@gmail.com>
2017-01-09 20:30:36 -0500

    Added Calculator.java file to compute numbers

---
+++ chapter_001/src/main/java/ru/job4j/Calculator.java
@@ -0,0 +1,59 @@
+package ru.job4j;
+
+/**
+ * Calculator class.
+ * @author Sergey Dubouski
+ * @version 1.0
+ * @since 07.01.2017
+*/
+class Calculator {
+	
+	/**
+	 * variable contains results of computation.
+	*/
+	private double result;
+	
+	/**
+	 * the method calculates addition of two numbers.
+	 * @param first - it's a first argument
+	 * @param second - it's a second argument
+	*/
+	public void add(double first, double second){
+		this.result = first + second;
+	}
+	
+	/**
+	 * the method calculates subtraction of two numbers.
+	 * @param first - it's a first argument
+	 * @param second - it's a second argument
+	*/
+	public void subtract(double first, double second){
+		this.result = first - second;		
+	}
+	
+	/**
+	 * the method calculates multiplication of two numbers.
+	 * @param first - it's a first argument
+	 * @param second - it's a second argument
+	*/
+	public void multiply(double first, double second){
+		this.result = first*second;		
+	}
+	
+	/**
+	 * the method calculates division of two numbers.
+	 * @param first - it's a first argument
+	 * @param second - it's a second argument
+	*/
+	public void divide(double first, double second){
+		this.result = first/second;		
+	}
+	
+	/**
+	 * the method returns the result of computation.
+	 * @return result.
+	*/
+	public double getResult(){
+		return this.result;
+	}
+}

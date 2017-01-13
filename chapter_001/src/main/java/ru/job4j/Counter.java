c04a34b1aafa3c683747ca3c978418fc0dc8cb58
Sergey Dubouski <sergeydubouski@gmail.com>
2017-01-12 21:15:41 -0500

    Added new class

---
+++ chapter_001/src/main/java/ru/job4j/Counter.java
@@ -0,0 +1,19 @@
+package ru.job4j;
+
+/**
+ * Counter class
+ * @author Sergey Dubouski
+ * @version 1.0
+ * @since 12.01.2017
+*/
+public class Counter {
+	/**
+	 * the mothod calcilates the sum of even values from a given range.
+	 * @param start - deginning value of the range.
+	 * @param finish - end value of the range.
+	 * @return - return the sum of even values.
+	*/
+	public int add(int start, int finish) {
+		
+	}
+}

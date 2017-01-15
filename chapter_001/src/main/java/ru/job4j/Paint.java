6678e07069c7d26ae243b14780f68d1d0e93c383
Sergey Dubouski <sergeydubouski@gmail.com>
2017-01-15 15:44:00 -0500

    commit

--- chapter_001/src/main/java/ru/job4j/Paint.java
+++ chapter_001/src/main/java/ru/job4j/Paint.java
@@ -7,15 +7,39 @@ package ru.job4j;
  * @since 14.01.2017
 */
 public class Paint {
-	
+
 	/**
 	 * the method draws a pyramid.
 	 * @param h - the pyramid's height.
 	*/
-	public String pyramid( int h) {
-		
-		for (int i = 0; i < h; i++) {
-			
+	public void paint(int h) {
+
+		StringBuilder stringPyramid = new StringBuilder();
+
+		//to create the pick of a triangle
+		for (int i = 0; i < h; i++ ) {
+			stringPyramid.append(" ");
 		}
-	} 
+		stringPyramid.append(String.format("^%s",System.getProperty("line.separator")));
+
+		//to create the left and right side of a triangle
+		for (int i = h; i < h * 2; i++) {
+			int k = 0;
+			for ( int j = 0; j < h * 2 - i - 1; j++) {
+				k++;
+				stringPyramid.append(" ");
+			}
+			stringPyramid.append("^");
+
+			for (int j = 0; j < i - k; j++) {
+				stringPyramid.append(" ");
+			}
+			stringPyramid.append(String.format("^%s",System.getProperty("line.separator")));
+		}
+
+		System.out.println(stringPyramid.toString());
+	}
 }
+
+
+

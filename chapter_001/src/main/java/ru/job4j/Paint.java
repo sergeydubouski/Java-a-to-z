c6e8c9ded148a923e6667a31bf62a783987b18af
Sergey Dubouski <sergeydubouski@gmail.com>
2017-01-14 23:07:28 -0500

    Initial commmit

---
+++ chapter_001/src/main/java/ru/job4j/Paint.java
@@ -0,0 +1,21 @@
+package ru.job4j;
+
+/**
+ * Paint class.
+ * @author Sergey Dubouski
+ * @version 1.0
+ * @since 14.01.2017
+*/
+public class Paint {
+	
+	/**
+	 * the method draws a pyramid.
+	 * @param h - the pyramid's height.
+	*/
+	public String pyramid( int h) {
+		
+		for (int i = 0; i < h; i++) {
+			
+		}
+	} 
+}
---
+++ chapter_001/src/main/java/ru/job4j/Pyramid.java
@@ -0,0 +1,11 @@
+package ru.job4j;
+
+/**
+ * Pyramid class.
+ * @author Sergey Dubouski
+ * @version 1.0
+ * @since 14.01.2017
+*/
+public class Pyramid {
+	
+}

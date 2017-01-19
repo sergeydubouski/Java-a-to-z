ac492ca9abd3e7c2070900b4bf0b60a082f85b12
Sergey Dubouski <sergeydubouski@gmail.com>
2017-01-18 21:25:05 -0500

    Updated functionality

--- chapter_001/src/main/java/ru/job4j/Turn.java
+++ chapter_001/src/main/java/ru/job4j/Turn.java
@@ -1,4 +1,27 @@
 package ru.job4j;
 
 /**
+ * Turn class.
+ * @author Sergey Dubouski
+ * @version 1.0
+ * @since 19.01.2017
 */
+public class Turn {
+	
+	int [] arrayTurn;
+	
+	public Turn (int first, second, third, fourth, fifth) {
+		
+		arrayTurn = new int [] {first, second, third, fourth, fifth};
+	}
+	
+	public void back () {
+		
+		for (int i = 0; i < arrayTurn.length; i ++) {
+			
+			int tempStorage = this.arrayTurn[i];
+			this.arrayTurn[i] = this.arrayTurn[arrayTurn.length - i - 1];
+			this.arrayTurn[arrayTurn.length - i - 1]  = tempStorage;			
+		}
+	}
+}

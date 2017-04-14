5c06fb01c007b8af2ffa020ce51e6fd2612d3641
Sergey Dubouski <sergeydubouski@gmail.com>
2017-04-14 06:44:38 -0400

    initial commit

---
+++ chapter_002/src/main/java/ru/job4j/shapes/Triangle.java
@@ -0,0 +1,22 @@
+package ru.job4j.shapes;
+
+/**
+ * class Triangle.
+ * @author Sergey Dubouski.
+ * @version 1.0.
+ * @since 12.04.2017
+*/
+class Triangle implements Shape {
+	/**
+	 * the method creates a geometric figure.
+	 * @return - shape of a geometric figure.
+	*/
+	public String pic() {
+		
+		return String.format("%4s*%s 
+							%2s*%1s*%s 
+							%1s*%3s*%s 
+							*******",
+							" ",System.getProperty("line.separator")," "," ",System.getProperty("line.separator")," "," ",System.getProperty("line.separator")));
+	}
+}

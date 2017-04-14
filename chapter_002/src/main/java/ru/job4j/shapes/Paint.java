325e1333193970c669bbacfbb3c67275e8c24395
Sergey Dubouski <sergeydubouski@gmail.com>
2017-04-14 06:54:45 -0400

    added new class Paint

---
+++ chapter_002/src/main/java/ru/job4j/shapes/Paint.java
@@ -0,0 +1,16 @@
+package ru.job4j.shapes;
+
+/**
+ * class Paint.
+ * @author Sergey Dubouski.
+ * @version 1.0.
+ * @since 12.04.2017
+*/
+class Paint {
+	/**
+	 * the method draws a geometric figure.
+	*/
+	public void draw(Shape shape) {
+		System.out.println(shape.pic());
+	}
+}

f04bdd77850ae65592572fbbf304e3998c473b1a
Sergey Dubouski <sergeydubouski@gmail.com>
2017-05-03 19:30:06 -0400

    added MenuOutOfRangeException

---
+++ chapter_002/src/main/java/ru/job4j/tracker/start/MenuOutOfRangeException.java
@@ -0,0 +1,16 @@
+package ru.job4j.tracker.start;
+
+/**
+ * class MenuOutOfRangeException.
+ * @author Sergey Dubouski.
+ * @version 1.0.
+ * @since 03.05.2017.
+*/
+public class MenuOutOfRangeException extends Exception {
+	/**
+	 * constructor.
+	*/
+	public MenuOutOfRangeException(String msg) {
+		super(msg);
+	}
+}
